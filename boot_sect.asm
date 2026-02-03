4e8952cfff11e51b77df04955865e6d85cb3fbb5
kiingxo <israelsoyombo@gmail.com>
2026-02-03 23:26:18 +0000

    Enhance boot sector code with screen clearing and cursor positioning

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,7 +1,22 @@
 [org 0x7c00]
 [bits 16]
 
+; Clear screen
+mov ah, 0x00
+mov al, 0x03
+int 0x10
+
+; Move cursor to center (row 12, col 38)
+mov ah, 0x02
+mov bh, 0x00
+mov dh, 12
+mov dl, 38
+int 0x10
+
+; Print "King" character by character
 mov ah, 0x0E
+mov bl, 0x0F    ; White text
+
 mov al, 'K'
 int 0x10
 
@@ -14,8 +29,9 @@ int 0x10
 mov al, 'g'
 int 0x10
 
-loop:
-    jmp loop
+; Infinite loop
+hang:
+    jmp hang
 
 times 510-($-$$) db 0
 dw 0xAA55
